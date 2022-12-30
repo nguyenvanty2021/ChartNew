d947afb831ef204a400b7c44074cd852301e1cc3
nguyenvanty2021 <nguyenvanty2021>
2022-12-30 11:13:19 +0700

    reduction per page

--- src/Api/coinApi.ts
+++ src/Api/coinApi.ts
@@ -13,7 +13,7 @@ const coinApi = {
     return axiosClient.get(url);
   },
   getAllMarkets(): Promise<any> {
-    const url = `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h`;
+    const url = `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h`;
     return axiosClient.get(url);
   },
   getCoinByName(query: string): Promise<any> {
